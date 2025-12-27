a03cf8fb7b6507d42f2f402a8625c5f8b10af6ae
Arman Hossain <armanhossain0477@gmail.com>
2025-12-27 18:27:15 +0600

    crud operation todos

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express"
 import { Pool } from "pg"
 import dotenv from "dotenv"
 import path from "path"
@@ -48,10 +48,17 @@ const initDB = async () => {
 
 initDB();
 
+// logger middleware 
+const logger = (req:Request, res:Response, next:NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n `)
+  next()
+}
 
-app.get('/', (req: Request, res: Response) => {
+
+app.get('/', logger, (req: Request, res: Response) => {
   res.send('Hello World!, Arman Hossain')
 })
+
 // users CRUD 
 app.post("/users", async (req: Request, res: Response) => {
 
@@ -94,27 +101,123 @@ app.get("/users", async (req: Request, res: Response) => {
 
 app.get("/users/:id", async (req: Request, res: Response) => {
   // console.log(req.params.id)
-  try{
-    const result = await pool.query(`SELECT * FROM users WHERE id = $1`,[req.params.id])
-    if(result.rows.length === 0){
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id])
+    if (result.rows.length === 0) {
       res.status(404).json({
         success: false,
         message: "user now found"
       })
     }
-    else{
+    else {
       res.status(200).json({
         success: true,
         message: "User fetched successfully",
         data: result.rows[0]
       })
     }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
+})
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+  // console.log(req.params.id)
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(`UPDATE users SET name=$1 , email=$2 WHERE id=$3 RETURNING *  `, [name, email, req.params.id])
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "user now found"
+      })
+    }
+    else {
+      res.status(200).json({
+        success: true,
+        message: "User updated successfully",
+        data: result.rows[0]
+      })
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
+})
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  // console.log(req.params.id)
+  try {
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [req.params.id])
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "user now found"
+      })
+    }
+    else {
+      res.status(200).json({
+        success: true,
+        message: "User deleted successfully",
+        data: result.rows
+      })
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
+})
+
+
+// todos CRUD
+app.post("/todos", async(req:Request, res:Response) => {
+  const {user_id, title} = req.body;
+  try{
+    const result = await pool.query(`INSERT INTO todos(user_id, title)VALUES ($1,$2) RETURNING *`, [user_id, title])
+    res.status(201).json({
+      success:true, 
+      message: "todos created successfully",
+      data: result.rows[0]
+    })
   }catch(err:any){
     res.status(500).json({
       success: false,
       message: err.message
     })
   }
+})
+
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`)
+    res.status(200).json({
+      success: true,
+      message: "todos retrive successfully ",
+      data: result.rows
+    })
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err
+    })
+  }
+})
+
+
+app.use((req, res)=> {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+    path: req.path
+  })
 })
 
 app.listen(port, () => {
