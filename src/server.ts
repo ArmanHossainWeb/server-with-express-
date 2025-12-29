4aa060eac002bc4c97460fae1fdc7292c8a94b31
Arman Hossain <armanhossain0477@gmail.com>
2025-12-29 20:36:48 +0600

    impletemnt put method

--- src/server.ts
+++ src/server.ts
@@ -183,10 +183,10 @@ app.post("/todos", async (req: Request, res: Response) => {
   const { user_id, title } = req.body;
 
   try {
-    const result = await pool.query(`INSERT INTO todos(user_id,title) VALUES($1,$2) RETURNING *`,[user_id,title])
+    const result = await pool.query(`INSERT INTO todos(user_id,title) VALUES($1,$2) RETURNING *`, [user_id, title])
     res.status(200).json({
-      success:true,
-      message:"todo created",
+      success: true,
+      message: "todo created",
       data: result.rows[0]
     })
 
@@ -198,47 +198,61 @@ app.post("/todos", async (req: Request, res: Response) => {
   }
 })
 
-app.get("/todos", async(req:Request , res:Response) =>{
+app.get("/todos", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(`SELECT * FROM todos `)
 
     res.status(200).json({
       success: true,
-      message:"todos retrived successfully",
+      message: "todos retrived successfully",
       data: result.rows
     })
-  } catch (err:any) {
+  } catch (err: any) {
     res.status(500).json({
-      success:false,
+      success: false,
       message: err.message,
       details: err
     })
   }
 })
 
-app.get("/todos/:id", async(req:Request, res: Response) => {
+app.get("/todos/:id", async (req: Request, res: Response) => {
   try {
-    const result = await pool.query(`SELECT * FROM todos WHERE id=$1`,[req.params.id]);
-    if(result.rows.length === 0){
-      return res.status(400).json({error:"todo not found"})
+    const result = await pool.query(`SELECT * FROM todos WHERE id=$1`, [req.params.id]);
+    if (result.rows.length === 0) {
+      return res.status(400).json({ error: "todo not found" })
     }
     res.json(result.rows[0])
-  } catch (err:any) {
+  } catch (err: any) {
     res.status(500).json({
-      error:"faild to fatch todos"
+      error: "faild to fatch todos"
     })
   }
 })
 
-app.put("/todos/:id",async(req:Request, res:Response)=> {
+app.put("/todos/:id", async (req, res) => {
+  const { title, completed } = req.body;
+
   try {
-    
-  } catch (err:any) {
-    res.status(500).json({
-       
-    })
+    const result = await pool.query(
+      "UPDATE todos SET title=$1, completed=$2 WHERE id=$3 RETURNING *",
+      [title, completed, req.params.id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Failed to update todo" });
   }
-})
+});
+
+
+
+
 
 app.use((req, res) => {
   res.status(404).json({
