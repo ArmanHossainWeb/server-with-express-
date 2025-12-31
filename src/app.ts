d8f4e6b6cc8656de248e540e408b4adf7a109511
Arman Hossain <armanhossain0477@gmail.com>
2025-12-31 12:40:37 +0600

    role based system implement

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import express, {  Request, Response } from "express"
+import config from "./config";
+import initDB from "./config/db";
+import logger from "./middleware/logger";
+import { userRoutes } from "./modules/user/user.routes";
+import { todoRoute } from "./modules/todo/todo.routes";
+import { authRoutes } from "./modules/auth/auth.routes";
+
+
+const app = express()
+
+// parser 
+app.use(express.json());
+
+
+// inititializing db 
+initDB();
+
+
+
+app.get('/', logger, (req: Request, res: Response) => {
+  res.send('Hello World!, Arman Hossain')
+})
+
+// users CRUD 
+app.use("/users", userRoutes);
+
+
+// todos CRUD
+app.use("/todos", todoRoute )
+
+// auth routes 
+app.use("/auth", authRoutes)
+
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+    path: req.path
+  })
+})
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,8 @@
-import express, {  Request, Response } from "express"
+
+import app from "./app"
 import config from "./config";
-import initDB from "./config/db";
-import logger from "./middleware/logger";
-import { userRoutes } from "./modules/user/user.routes";
-import { todoRoute } from "./modules/todo/todo.routes";
-import { authRoutes } from "./modules/auth/auth.routes";
 
-
-const app = express()
 const port = config.port;
-// parser 
-app.use(express.json());
-
-
-// inititializing db 
-initDB();
-
-
-
-app.get('/', logger, (req: Request, res: Response) => {
-  res.send('Hello World!, Arman Hossain')
-})
-
-// users CRUD 
-app.use("/users", userRoutes);
-
-
-// todos CRUD
-app.use("/todos", todoRoute )
-
-// auth routes 
-app.use("/auth", authRoutes)
-
-
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Route Not Found",
-    path: req.path
-  })
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
